ebb4b18ce471febf0c5747636187a4d491c17937
d0422 <rlfehd2013@naver.com>
2023-03-17 15:17:18 +0900

    variable, final, late, const

---
+++ main.dart
@@ -0,0 +1,8 @@
+void main() {
+  final String name = "donggil";
+  late final int age; //이후에 값 등록
+  const constantVar = "1"; // const는 컴파일 타임에 알고있는 값이어야함 -> fetch해주는데이터면 안됨
+  age = 24;
+  print(name);
+  print(age);
+}
